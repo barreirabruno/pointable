83181c52a81398eccd087231bbf6655e4f9450e4
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-08 13:35:06 -0300

    test: ensure it defines publishedAt attribute on Poll instance

--- tests/domain/entities/poll-entity.spec.ts
+++ tests/domain/entities/poll-entity.spec.ts
@@ -62,4 +62,31 @@ describe('Poll Entity', () => {
       content: 'any_description_content_value'
     })
   })
+
+  it('should define publishedAt attribute on Poll instance', () => {
+    const fixedDate = new Date(Date.now())
+    const params = {
+      title: 'any_poll_title',
+      type: 1,
+      published: 0,
+      createdAt: fixedDate,
+      updatedAt: fixedDate,
+      publishedAt: fixedDate,
+      startsAt: fixedDate,
+      endsAt: fixedDate,
+      content: 'any_description_content_value'
+    }
+    sut = new Poll(params)
+    expect(sut).toEqual({
+      title: 'any_poll_title',
+      type: 1,
+      published: 0,
+      createdAt: fixedDate,
+      updatedAt: fixedDate,
+      publishedAt: fixedDate,
+      startsAt: fixedDate,
+      endsAt: fixedDate,
+      content: 'any_description_content_value'
+    })
+  })
 })
