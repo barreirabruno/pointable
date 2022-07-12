857ac5228e3ef87ba874f54d2b3b0537968e3038
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-11 22:04:47 -0300

    chore: add success http helper

--- src/application/helpers/http-helpers.ts
+++ src/application/helpers/http-helpers.ts
@@ -9,3 +9,8 @@ export const serverError = (error: Error): HttpResponse<Error> => ({
   statusCode: 500,
   data: new ServerError(error)
 })
+
+export const success = <T = any>(data: T): HttpResponse<T> => ({
+  statusCode: 200,
+  data: data
+})
