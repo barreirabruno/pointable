f7c63ba3d27738754735596d9b96fb38d701acd0
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-11 21:34:15 -0300

    test: ensure it returns 500 if perform  method fail for any reason

--- tests/presentation/controller.spec.ts
+++ tests/presentation/controller.spec.ts
@@ -1,10 +1,11 @@
 import Controller from '@/application/controllers/controller'
 import { HttpResponse } from '@/application/helpers/http-helpers'
+import ServerError from '@/domain/errors/server-error.domain'
 
 class ControllerStub extends Controller {
   result: HttpResponse = {
     statusCode: 200,
-    body: 'any_data'
+    data: 'any_data'
   }
 
   async perform (httpRequest: any): Promise<HttpResponse<any>> {
@@ -19,11 +20,30 @@ describe('Controller', () => {
     sut = new ControllerStub()
   })
 
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should return 500 if perform  method fail for any reason', async () => {
+    const error = new ServerError(new Error('[ANY_ERROR]'))
+    jest.spyOn(sut, 'perform').mockResolvedValueOnce({
+      statusCode: 500,
+      data: error
+    })
+
+    const httpResponse = await sut.handle({ key: 'any_value' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 500,
+      data: error
+    })
+  })
+
   it('should return same result as perform', async () => {
     const request = await sut.handle({ key: 'any_value' })
     expect(request).toEqual({
       statusCode: 200,
-      body: 'any_data'
+      data: 'any_data'
     })
   })
 })
