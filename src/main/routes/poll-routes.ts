0c260f93b24388fa646eb56e4e68fe6fe48582ee
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-13 23:56:34 -0300

    feat: call create poll controller

--- src/main/routes/poll-routes.ts
+++ src/main/routes/poll-routes.ts
@@ -1,10 +1,23 @@
 import { FastifyInstance, FastifyServerOptions } from 'fastify'
 import { makeCreatePollController } from '../factories/poll-controller'
 
+interface ICreatePoll {
+  title: string
+  type: number
+  published: number
+  createdAt: string
+  updatedAt: string
+  startsAt: string
+  endsAt: string
+  content: string
+}
+
 module.exports = function (app: FastifyInstance, opts: FastifyServerOptions, done: any) {
   const createPollController = makeCreatePollController()
-  app.get('/poll', async function (request, reply) {
-    const httpResponse = await createPollController.handle(request)
+  app.post<{
+    Body: ICreatePoll
+  }>('/poll', async function (request, reply) {
+    const httpResponse = await createPollController.handle(request.body)
     void reply.status(httpResponse.statusCode).send(httpResponse)
   })
   done()
